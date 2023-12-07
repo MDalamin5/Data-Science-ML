a77b24a6b72e4bca923db3777ef90531f4d10776
MDalamin5 <mdal.amin5@northsouth.edu>
2023-12-07 11:23:31 +0600

    autogui in py

---
+++ Intro-of-PY/Phitron/Week-1/Module-3/autotype.py
@@ -0,0 +1,11 @@
+import pyautogui
+from time import sleep
+
+# pyautogui.write('Hello dlkajsf', interval= 0.15)
+# pyautogui.press('enter')
+
+
+# for i in range(10):
+#     pyautogui.write('Hello dlkajsf', interval= 0.15)
+#     pyautogui.press('enter')
+
---
+++ Intro-of-PY/Phitron/Week-1/Module-3/build-in-modules-fun.py
@@ -0,0 +1,26 @@
+from math import *
+from random import *
+from time import sleep
+num = ceil(4.6)
+print(num)
+
+num2 = floor(5.8)
+print(num2)
+
+num = [3, 4, 6, 4]
+print(gcd(10, 20, 30, 50))
+print(lcm(20, 40, 60, 5))
+
+random_num = int(random() * 10)
+print(random_num)
+
+print(randint(100, 102))
+
+list = [1, 2, 4, 5, 6, 7, 8, 4]
+print(choice(list))
+print(choice(['alamin', 'aminul', 'niloy']))
+
+for item in list:
+    print(item)
+    sleep(0.7)
+
